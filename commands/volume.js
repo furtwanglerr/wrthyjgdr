cf3b34039d20f32394e3244210f4ee75f1c31c87
sinjaeXD <140227918+sinjaeXD@users.noreply.github.com>
2024-02-19 20:18:09 +0900

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -4,11 +4,11 @@ const db = require("../mongoDB");
 
 module.exports = {
   name: "volume",
-  description: "Allows you to adjust the music volume.",
+  description: "음악볼륨",
   permissions: "0x0000000000000800",
   options: [{
     name: 'volume',
-    description: 'Type the number to adjust the volume.',
+    description: '음악볼륨',
     type: ApplicationCommandOptionType.Integer,
     required: true
   }],
@@ -17,25 +17,25 @@ module.exports = {
     try {
       const queue = client.player.getQueue(interaction.guild.id);
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ 재생중인 음악 없음', ephemeral: true });
       }
 
       const vol = parseInt(interaction.options.getInteger('volume'));
 
       if (!vol) {
         return interaction.reply({
-          content: `Current volume: **${queue.volume}** 🔊\nTo change the volume, type a number between \`1\` and \`${maxVol}\`.`,
+          content: `현재볼륨: **${queue.volume}** 🔊\n볼륨지정: \`1\` ~ \`${maxVol}\`.`,
           ephemeral: true
         });
       }
 
       if (queue.volume === vol) {
-        return interaction.reply({ content: 'Current volume is already set to **' + vol + '**!', ephemeral: true });
+        return interaction.reply({ content: '현재볼륨은 이미 **' + vol + '**입니다!', ephemeral: true });
       }
 
       if (vol < 1 || vol > maxVol) {
         return interaction.reply({
-          content: `Please type a number between \`1\` and \`${maxVol}\`.`,
+          content: `볼륨을 다음 범위 안으로 설정해주세요: \`1\` ~ \`${maxVol}\`.`,
           ephemeral: true
         });
       }
@@ -46,15 +46,15 @@ module.exports = {
         const embed = new EmbedBuilder()
           .setColor('#d291fe')
           .setAuthor({
-        name: 'Your Music! Your Rules!',
+        name: '음악',
         iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157528025739563088/5657-volume-icon.png?ex=6518ef7b&is=65179dfb&hm=1797c2830537a28b5c6a57564517cc509146d02383a69fb4239d7b5d55aceeed&', 
         url: 'https://discord.gg/FUEHs7RCqz'
     })
-          .setDescription(`**Adjusting Volume : ** **${vol}/${maxVol}**`);
+          .setDescription(`**볼륨 설정중 : ** **${vol}/${maxVol}**`);
 
         return interaction.reply({ embeds: [embed] });
       } else {
-        return interaction.reply({ content: '❌ Something went wrong while changing the volume.', ephemeral: true });
+        return interaction.reply({ content: '❌ 볼륨 조정중 에러남', ephemeral: true });
       }
     } catch (e) {
       console.error(e);
