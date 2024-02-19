6328dde01af87c4cf570558b11738b131806321f
sinjaeXD <140227918+sinjaeXD@users.noreply.github.com>
2024-02-19 21:10:24 +0900

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -3,7 +3,7 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "resume",
-  description: "Start paused music.",
+  description: "닫힌 음악 다시 재생",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -12,11 +12,11 @@ module.exports = {
 
     try {
       if (!queue) {
-        return interaction.reply({ content: '⚠️ Queue is empty!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ 큐가 빔!', ephemeral: true });
       }
 
       if (!queue.paused) {
-        return interaction.reply({ content: '⚠️ No paused music!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ 닫힌 음악이 없음!', ephemeral: true });
       }
 
       const success = queue.resume();
@@ -24,11 +24,11 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#7645fe')
         .setAuthor({
-          name: 'Song Resumed',
+          name: '음악 다시 재생함',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157296313549983846/8929-purple-play-icon.png?ex=651817ae&is=6516c62e&hm=55fc041718da9277d1cdb13ef25ebf043d90588ee33c4bc838d9634ecfbc8e99&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-        .setDescription(success ? '**The music springs back to life!!**' : '❌ Error: Unable to resume song')
+        .setDescription(success ? '**음악 다시 재생함!**' : '❌ 에러: 음악을 다시 재생할 수 없음')
         
 
       return interaction.reply({ embeds: [embed] });
