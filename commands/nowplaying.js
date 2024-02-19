4a461c8f73d83082372954a97d7b56e4e33c8f7c
sinjaeXD <140227918+sinjaeXD@users.noreply.github.com>
2024-02-19 20:49:54 +0900

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -2,28 +2,28 @@ const { EmbedBuilder } = require('discord.js');
 const db = require("../mongoDB");
 module.exports = {
   name: "nowplaying",
-  description: "get info of current song.",
+  description: "현재 재생중인 음악 정보",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
     try {
 
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ 재생중인 음악 없음!`, ephemeral: true }).catch(e => { })
 
       const track = queue.songs[0];
-      if (!track) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+      if (!track) return interaction.reply({ content: `⚠️ 재생중인 음악 없음!`, ephemeral: true }).catch(e => { })
 
       const embed = new EmbedBuilder();
       embed.setColor(client.config.embedColor);
       embed.setThumbnail(track.thumbnail);
       embed.setTitle(track.name)
-      embed.setDescription(`> **Audio** \`%${queue.volume}\`
-> **Duration :** \`${track.formattedDuration}\`
-> **URL :** **${track.url}**
-> **Loop Mode :** \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'}\`
-> **Filter**: \`${queue.filters.names.join(', ') || 'Off'}\`
-> **By :** <@${track.user.id}>`);
+      embed.setDescription(`> **오디오** \`%${queue.volume}\`
+> **재생시간 :** \`${track.formattedDuration}\`
+> **링크 :** **${track.url}**
+> **반복재생모드 :** \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'}\`
+> **필터**: \`${queue.filters.names.join(', ') || 'Off'}\`
+> **재생한사람 :** <@${track.user.id}>`);
 
 
       interaction.reply({ embeds: [embed] }).catch(e => { })
