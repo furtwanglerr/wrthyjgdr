fbe459b3d6d202ac518d5b95ef86ff1d20c0b36f
sinjaeXD <140227918+sinjaeXD@users.noreply.github.com>
2024-02-19 21:13:11 +0900

    Update shuffle.js

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -3,17 +3,17 @@ const config = require("../config.js");
 const db = require("../mongoDB");
 module.exports = {
   name: "shuffle",
-  description: "Shuffle the queue songs",
+  description: "큐에 있는 음악들 섞기",
   options: [],
   permissions: "0x0000000000000800",
   run: async (client, interaction) => {
     try {
 
         const queue = client.player.getQueue(interaction.guild.id);
-        if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+        if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ 재생중인 음악 없음!`, ephemeral: true }).catch(e => { })
         try {
           queue.shuffle(interaction)
-        return interaction.reply({ content: `<@${interaction.user.id}>, Shuffled the songs for You!!` }).catch(e => { })
+        return interaction.reply({ content: `<@${interaction.user.id}>, 음악섞음!` }).catch(e => { })
         } catch(err) {
         return interaction.reply({ content: `**${err}**` }).catch(e => { })
         }
