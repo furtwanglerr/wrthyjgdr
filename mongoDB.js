5fd972e35ab400bbbb1cd79d9ffad325ffab7729
furtwanglerr <140227918+furtwanglerr@users.noreply.github.com>
2024-05-12 14:06:06 +0900

    Update mongoDB.js

--- mongoDB.js
+++ mongoDB.js
@@ -21,17 +21,5 @@ module.exports = {
   playlist: model("playlist", playlist)
 };
 /*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+asdasdasd0A
 */
